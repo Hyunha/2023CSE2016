495ff7d8dac5b75dd79ec103680b85109cf1da2d
Hyunha Kim <lagrato@gmail.com>
2023-11-05 22:35:15 +0900

    skeleton codes for lab#08.

---
+++ labs/code08/Dealer.java
@@ -0,0 +1,17 @@
+
+public class Dealer {
+	
+	CardDeck deck;
+	
+	public Dealer() {
+		deck = new CardDeck();
+	}
+	
+	public void dealTo(CardPlayerBehavior p) {
+		// ...
+	}
+	
+	public void dealOneTo(CardPlayerBehavior p) {
+		// ...
+	}
+}
